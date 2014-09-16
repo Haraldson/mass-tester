2413d882217c4a97f5b5b1366bc243f2fc04cdfe
Hein Haraldson Berg <hein@agens.no>
2014-09-16 14:09:36 +0200

    Fixes and styles?

--- js/app/ua-parser/display.js
+++ js/app/ua-parser/display.js
@@ -1,10 +1,11 @@
 $(function()
 {
     var parser = new UAParser();
-    var browser = parser.getBrowser();
-    var engine = parser.getEngine();
     var os = parser.getOS();
     var device = parser.getDevice();
 
-    $('#mass-tester').prepend('<div id="meta">' + os + ' ' + device + '</div>');
+    $('#mass-tester').prepend('<div id="meta">
+        <p>' + os.name + ' ' + os.version + '</p>
+        <p>' + device.vendor + ' ' + device.model + '</p>
+    </div>');
 });
