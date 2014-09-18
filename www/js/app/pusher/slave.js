1e97ceb65b58896ce023b8fbacbba452cf3800f7
Hein Haraldson Berg <hein@agens.no>
2014-09-18 14:42:03 +0200

    Testing

--- www/js/app/pusher/slave.js
+++ www/js/app/pusher/slave.js
@@ -1,13 +1,20 @@
 $(function()
 {
-    var pusher = new Pusher('15e714e019131aecff4a');
-    var commands = pusher.subscribe('private-mass-tester');
-
     var $body = $('body');
     $body.on('animationend webkitAnimationEnd', function()
     {
         $(this).removeClass('flash');
     });
+
+    var open = function(data)
+    {
+        $body.addClass('flash');
+        window.location = data.url;
+        window.setTimeout(window.location.reload, 500);
+    };
+
+    var pusher = new Pusher('15e714e019131aecff4a');
+    var commands = pusher.subscribe('private-mass-tester');
 
     // Show a web page
     commands.bind('client-show', function(data)
@@ -16,9 +23,5 @@ $(function()
     });
 
     // Open a URL directly
-    commands.bind('client-open', function(data)
-    {
-        $body.addClass('flash');
-        window.location = data.url;
-    });
+    commands.bind('client-open', open);
 });
