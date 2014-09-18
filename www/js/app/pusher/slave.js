9becd05ac2940234f102cbecbe88aa81c91286a5
Hein Haraldson Berg <hein@agens.no>
2014-09-18 14:15:08 +0200

    Visuale received events

--- www/js/app/pusher/slave.js
+++ www/js/app/pusher/slave.js
@@ -3,6 +3,12 @@ $(function()
     var pusher = new Pusher('15e714e019131aecff4a');
     var commands = pusher.subscribe('private-mass-tester');
 
+    $body = $('body');
+    $body.on('animationend webkitAnimationEnd', function()
+    {
+        $(this).removeClass('flash');
+    });
+
     // Show a web page
     commands.bind('client-show', function(data)
     {
@@ -12,8 +18,10 @@ $(function()
     // Open a URL directly
     commands.bind('client-open', function(data)
     {
+        $body.addClass('flash');
+        
         $('<a href="' + data.url + '"></a>')
-            .appendTo('body')
+            .appendTo($body)
             .click()
             .delay(200)
             .remove();
